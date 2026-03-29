f5714ace68dec6941a89d394a043a4e5824eddb7
Shubhu09-cmyk <shinesonker202@gmail.com>
2026-03-29 21:30:27 +0530

    Add: string-encode-and-decode - submission-2

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-2.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        string result;
+        for(string& str : strs){
+            result+=to_string(str.size())+"#"+str;    //O(n) time
+        }
+        return result;
+    }
+
+    vector<string> decode(string s) {
+        vector<string> result;
+       
+        int i=0;
+        
+        while(i<s.size()){
+                int j=i;
+                //finding #
+                while(s[j]!='#'){
+                    j++;
+                }
+                //extract length
+               int len=stoi(s.substr(i,j-i));
+                //extracting worf
+              string str=s.substr(j+1,len);
+                result.push_back(str);     //O(n) time
+
+                //movement of i for another word  in vector
+                i=j+1+len;
+
+
+        }
+return result;
+    }
+};
