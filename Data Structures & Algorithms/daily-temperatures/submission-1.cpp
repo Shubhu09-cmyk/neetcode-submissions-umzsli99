5b489d37f9df6c07e7ee241aa5a94ddbeceadac3
Shubhu09-cmyk <shinesonker202@gmail.com>
2026-04-01 21:04:03 +0530

    Add: daily-temperatures - submission-1

---
+++ Data Structures & Algorithms/daily-temperatures/submission-1.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        vector<int> res(temperatures.size(),0);
+        stack<pair<int,int>> st;
+
+        for(int i=0;i<temperatures.size();i++){
+           // int t=temperatures[i];
+            
+            while(!st.empty() && temperatures[i]>st.top().first) //if temperature[i] is greater than stack top element
+            {
+                int index_pair=st.top().second;
+                st.pop();
+                res[index_pair]=i-index_pair;    //computes number of days waited
+            }
+            //if temperature[i] is <= than stack top element then push the element only
+            st.push({temperatures[i],i});
+        }
+       return res; 
+    }
+};
