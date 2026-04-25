cfcc1b36a938e8abc65676b92738070c93e9decd
Shubhu09-cmyk <shinesonker202@gmail.com>
2026-04-26 00:09:42 +0530

    Add: surrounded-regions - submission-3

---
+++ Data Structures & Algorithms/surrounded-regions/submission-3.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+   vector<pair<int,int>> directions={{0,-1},{-1,0},{0,1},{1,0}};
+    void solve(vector<vector<char>>& board) {
+        //solving using dfs
+        int n=board.size(); //rows
+        int m=board[0].size(); //col
+        if (board.empty() || board[0].empty()) return;    //edge case
+        vector<vector<int>> vis(n,vector<int>(m,0));
+
+        //checking the 1st and rast row
+        for(int i=0;i<m;i++){ 
+           if(board[0][i]=='O'){//first row
+            dfs(0,i,vis,board);
+           }
+           if(board[n-1][i]=='O'){//last row
+           dfs(n-1,i,vis,board);
+           }
+        }
+        //checking 1st and last column
+        for(int i=0;i<n;i++){
+            //1st column
+            if(board[i][0]=='O'){//first col
+                dfs(i,0,vis,board);
+            }
+            //last col
+            if(board[i][m-1]=='O'){//last col
+            dfs(i,m-1,vis,board);
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+            if(board[i][j]=='O' && vis[i][j]==0){
+            board[i][j]='X';
+        }
+            }
+        }
+        
+        
+    }
+
+    private:
+     void dfs(int i, int j,vector<vector<int>>& vis,vector<vector<char>>& board){
+        vis[i][j]=1;
+        for(auto [x,y]:directions){
+            int ni=i+x;
+            int nj=j+y;
+            if(ni>=0 && nj>=0 && ni<board.size() && nj<board[0].size() && vis[ni][nj]==false && board[ni][nj]=='O'){
+               dfs(ni,nj,vis,board) ;
+            }
+        }
+    }
+};
