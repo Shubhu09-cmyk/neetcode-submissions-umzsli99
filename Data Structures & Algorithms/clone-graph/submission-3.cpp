1ea8a72859159c1a6b81d515b5023e0849fa0e16
Shubhu09-cmyk <shinesonker202@gmail.com>
2026-04-26 12:55:27 +0530

    Add: clone-graph - submission-3

---
+++ Data Structures & Algorithms/clone-graph/submission-3.cpp
@@ -0,0 +1,56 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+    private:
+    Node* dfs(Node* curr,unordered_map<Node*,Node*>& mp){   //better make map as global parameter so no need to pass everytime
+       //if curr already exist in map
+        if(mp.count(curr)){
+            return mp[curr];
+        }
+        //if nbr not exist , create clone and save in map
+            Node* clone=new Node(curr->val);
+            mp[curr]=clone;
+        
+
+        vector<Node*> neighbor;
+        for(Node* nbr:curr->neighbors){
+            
+                neighbor.push_back(dfs(nbr,mp));
+            
+        }
+        clone->neighbors=neighbor;
+
+        /*for(Node* nbr : curr->neighbors){
+    clone->neighbors.push_back(dfs(nbr, mp));
+}. can use this also . don't create vector as it takes extra space
+*/
+        return clone;
+    }
+public:
+    
+    Node* cloneGraph(Node* node) {
+        unordered_map<Node*,Node*> mp;
+        if(!node) return nullptr;
+        return dfs(node,mp);
+     }
+
+};
