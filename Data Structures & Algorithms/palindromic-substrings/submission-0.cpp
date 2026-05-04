1e2d9871d425e9885f60827d911297adf0514bf3
Shubhu09-cmyk <shinesonker202@gmail.com>
2026-05-04 19:12:04 +0530

    Add: palindromic-substrings - submission-0

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+private:
+    int palindrome(string& s,int left,int right){
+         int count=0;
+        while(left>=0 && right<s.length() && s[left]==s[right]){
+            count++;
+            left--;
+            right++;
+        }
+        return count;
+    }
+public:
+   
+    int countSubstrings(string s) {
+         int count=0;
+        for(int i=0;i<s.length();i++){
+            //odd length palindrome
+            count+=palindrome(s,i,i);   //left and right index are same so points at centre
+            //even length palindrome
+            count+=palindrome(s,i,i+1); //left and right diff even
+        }     
+        return count;  
+    }
+};
