cba065acfd86f7cbee97f8d08951e7511ee8e7b8
Shubhu09-cmyk <shinesonker202@gmail.com>
2026-05-05 20:29:30 +0530

    Add: coin-change - submission-1

---
+++ Data Structures & Algorithms/coin-change/submission-1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+    //TC O(mxn). SC O(amount)
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> dp(amount+1,amount+1);   //INT_MAX. each element infinity
+        dp[0]=0;
+        for(int i=1;i<=amount;i++){  //here dp[i] is amount
+        for(int coin:coins){
+            if(i>=coin){
+                dp[i]=min(dp[i],dp[i-coin]+1);//here 1 is coin itself like 1,5 or 10
+            }
+        }
+
+        }
+       return (dp[amount]>amount)?-1:dp[amount]; //dp[amount]==INT_MAX)?-1:dp[amount]
+    }
+};
