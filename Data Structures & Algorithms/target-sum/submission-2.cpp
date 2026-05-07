b861243669ae8626f96d276e2632496e12468241
Shubhu09-cmyk <shinesonker202@gmail.com>
2026-05-07 11:02:13 +0530

    Add: target-sum - submission-2

---
+++ Data Structures & Algorithms/target-sum/submission-2.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int totalSum=0;
+        for(int x : nums){
+          totalSum+=x;  
+        }
+        if(abs(target)>totalSum) return 0;  //impossible case
+        if((target+totalSum)%2==1) return 0;//if sum -ve. then can't find subset
+        int subsetSum=(target+totalSum)/2;
+        vector<int> dp(subsetSum+1,0);
+        dp[0]=1;  //for sum to be 0; only one way possible
+        for(int num:nums){
+            for(int j=subsetSum;j>=num;j--){
+                dp[j]+=dp[j-num];
+            }
+        }
+        return dp[subsetSum];
+    }
+};
