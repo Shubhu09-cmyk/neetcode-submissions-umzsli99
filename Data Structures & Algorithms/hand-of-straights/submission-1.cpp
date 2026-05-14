1963f411c3ead8443eb71d69bc7692457ce1128b
Shubhu09-cmyk <shinesonker202@gmail.com>
2026-05-14 11:55:31 +0530

    Add: hand-of-straights - submission-1

---
+++ Data Structures & Algorithms/hand-of-straights/submission-1.cpp
@@ -0,0 +1,23 @@
+class Solution {
+    //TC O(n log n ). Space O(1)
+public:
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+   if(hand.size()%groupSize!=0) return false;  //return false if we can't form groups
+   unordered_map<int,int> freq;
+   for(int x:hand){
+    freq[x]++; //calculate freq
+   }
+   //sort the hand array
+   sort(hand.begin(),hand.end());
+   for(int x:hand){
+    if(freq[x]==0) continue;//element already used
+    for(int i=0;i<groupSize;i++){
+        int curr=x+i;
+        if(freq[curr]==0) return false;//required consecutive card does not exiost
+
+        freq[curr]--;
+    }
+   }
+    return true;
+    }
+};
