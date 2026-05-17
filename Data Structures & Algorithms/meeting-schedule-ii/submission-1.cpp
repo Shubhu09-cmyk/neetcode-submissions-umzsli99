cf066d81aab709c2698e9a617033bfdbb995e044
Shubhu09-cmyk <shinesonker202@gmail.com>
2026-05-17 19:13:10 +0530

    Add: meeting-schedule-ii - submission-1

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-1.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+    //time O(n log n). space O(n)
+public:
+    int minMeetingRooms(vector<Interval>& intervals) {
+        if(intervals.empty()) return 0;
+        map<int,int> events;
+        for(auto& meeting:intervals){
+            events[meeting.start]++;//interval is class
+            events[meeting.end]--;
+        }
+        int overlap=0;
+        int maxOverlap=0;
+        for(auto& pq:events){
+            overlap+=pq.second;
+            maxOverlap=max(maxOverlap,overlap);
+       }
+        
+     return maxOverlap;   
+    }
+};
