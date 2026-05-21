fe334bc77c2e72a187fda1b715f3d3883ddd01c7
Shubhu09-cmyk <shinesonker202@gmail.com>
2026-05-21 22:33:31 +0530

    Add: reverse-integer - submission-1

---
+++ Data Structures & Algorithms/reverse-integer/submission-1.cpp
@@ -0,0 +1,15 @@
+class Solution {
+    //tc o(log10 x) sc O(1)
+public:
+    int reverse(int x) {
+       long long rev=0;
+       while(x!=0){
+        int digit=x%10;
+        x=x/10;
+        if(rev>INT_MAX/10 || (rev==INT_MAX/10 && digit>7)) return 0;
+        if(rev<INT_MIN/10 || (rev==INT_MIN/10 && digit<-8)) return 0;
+        rev=rev*10+digit;
+       }
+        return (int)rev;
+    }
+};
